f4d8219c5db771618388d1efc7c333b9f367fa01
JPlante9117 <jacrplante@gmail.com>
2020-03-03 09:26:33 -0500

    Done.

--- src/reducers/quotes.js
+++ src/reducers/quotes.js
@@ -5,28 +5,40 @@ export default (state = [], action) => {
       return [...state, action.quote]
     
     case "REMOVE_QUOTE":
-      idx = state.findIndex(quote => quote.id === action.quoteId)
-      return [...state.slice(0, idx), ...state.slice(idx + 1)]
+      return state.filter(quote => quote.id !== action.quoteId)
 
     case "UPVOTE_QUOTE":
       idx = state.findIndex(quote => quote.id === action.quoteId)
       quote = state[idx]
-
-      return [
-        ...state.slice(0, idx),
-        Object.assign({}, quote, {votes: quote.votes += 1}),
-        state.slice(idx + 1)
-      ]
+      if (quote !== state[state.length - 1]) {
+        return [
+          ...state.slice(0, idx),
+          Object.assign({}, quote, {votes: quote.votes += 1}),
+          state.slice(idx + 1)
+        ]
+      } else {
+        return [
+          ...state.slice(0, idx),
+          Object.assign({}, quote, {votes: quote.votes += 1})
+        ]
+      }
 
     case "DOWNVOTE_QUOTE":
       idx = state.findIndex(quote => quote.id === action.quoteId)
       quote = state[idx]
       if (quote.votes > 0){
-        return [
-          ...state.slice(0, idx),
-          Object.assign({}, quote, {votes: quote.votes -= 1}),
-          state.slice(idx + 1)
-        ]
+        if (quote !== state[state.length - 1]){
+          return [
+            ...state.slice(0, idx),
+            Object.assign({}, quote, {votes: quote.votes -= 1}),
+            state.slice(idx + 1)
+          ]
+        } else {
+          return [
+            ...state.slice(0, idx),
+            Object.assign({}, quote, {votes: quote.votes -= 1})
+          ]
+        }
       }
       return state
 
